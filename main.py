494506c2533d7a2392e09b09d33ae460b0139cd9
Rares <nikn7599@gmail.com>
2025-02-17 22:25:43 +0200

    primele

---
+++ main.py
@@ -0,0 +1,15 @@
+import flet as ft
+
+from pages.login import Login
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(ft.Text(value="Hello, world!"))
+
+ft.app(main)
+
+md davinki2025
+cd davinki2025
+python -m venv .venv
+.venv\Scripts\activate
